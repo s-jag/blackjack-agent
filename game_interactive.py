d97d3100ff4e5af4edb215be267cb8c6fcc840b5
Arnav Chopra <arnav.chopra@gmail.com>
2024-03-13 15:59:16 -0400

    working round

---
+++ game_interactive.py
@@ -0,0 +1,227 @@
+import random
+import time
+
+SUITS = ['♠', '♥', '♦', '♣']
+VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+class Card:
+    def __init__(self, suit: str, value: str, show=True):
+        self.suit = suit
+        self.value = value
+        self.show = show
+
+    def __str__(self):
+        return f'{self.suit} {self.value}' if self.show else 'X'
+    
+class Deck:
+    def __init__(self, number_of_decks=3):
+        one_deck = [Card(s, v) for s in SUITS for v in VALUES]
+        self.cards = []
+        for _ in range(number_of_decks):
+            self.cards += one_deck
+        random.shuffle(self.cards)
+
+    def draw(self, show=True):
+        card = self.cards.pop()
+        card.show = show
+        return card
+    
+class Hand:
+    def __init__(self, cards=None):
+        if not cards:
+            cards = []
+        self.cards = cards
+        self.value = 0
+
+    def clear(self):
+        self.cards = []
+        self.value = 0
+    
+    def reveal(self):
+        showed = False
+        for card in self.cards:
+            if not card.show:
+                showed = True
+            card.show = True
+        return showed
+
+    def add_to_hand(self, card: Card):
+        self.cards.append(card)
+        if self.compute_value() > 21:
+            self.value = 0
+            return True
+        return False
+    
+    def compute_value(self):
+        value_high = 0
+        value_low = 0
+        for card in self.cards:
+            if not card.show:
+                continue
+            if card.value == 'A':
+                value_high += 11
+                value_low += 1
+                continue
+            value_high += min(1 + VALUES.index(card.value), 10)
+            value_low += min(1 + VALUES.index(card.value), 10)
+        self.value = value_high if value_high < 21 else value_low
+        return self.value
+    
+    def __str__(self):
+        return '[' + ', '.join(map(str, self.cards)) + ']'
+    
+    def __repr__(self):
+        return '[' + ', '.join(map(str, self.cards)) + ']'
+
+class Player:
+    def __init__(self, id:int, money=100, bet=1):
+        self.hands = [Hand()]
+        self.hand_idx = 0
+        self.money = money
+        self.bet = bet
+        self.playing = True if money > 0 else False
+        self.doubled = False
+        self.id = id
+
+    def play(self, dealer_hand: Hand, card: Card): # where the player logic comes in
+        if not self.playing:
+            print(f'Player {self.id} is not playing.')
+            return False
+        print(f'Player {self.id} is about to play.')
+        move = input('Press h to hit, x to split, s to stay and d to double: ')
+        match move:
+            case 'h':
+                self.hit(card)
+            case 'd':
+                print('here')
+                self.double()
+                card.show = False
+                self.hit(card)
+            case 'x':
+                self.split(card)
+            case 's':
+                self.stand()
+        time.sleep(1)
+        return True
+
+    def is_playing(self):
+        return self.is_playing
+    
+    def add_to_hand(self, card: Card):
+        return self.hands[self.hand_idx].add_to_hand(card)
+    
+    def reset(self):
+        self.hands = [Hand()]
+        self.hand_idx = 0
+        self.doubled = False
+
+    def hit(self, card: Card):
+        if not self.playing:
+            return
+        bust = self.add_to_hand(card)
+        print(f'Player {self.id} has chosen to hit on {card}. Total score is {self.hands[self.hand_idx].compute_value()}. Hand is {self.hands}.')
+        time.sleep(1)
+        if bust:
+            print(f'Player\'s hand number {self.hand_idx + 1} went bust. Player loses {self.bet} money. Player dumb.')
+            self.money -= self.bet
+            self.playing = False
+        if self.doubled:
+            self.stand()
+    
+    def stand(self):
+        print(f'Player {self.id} has chosen to stand on hand {self.hand_idx + 1}. Total score is {self.hands[self.hand_idx].compute_value()}. Hand is {self.hands}.')
+        self.hand_idx += 1
+        if self.hand_idx >= len(self.hands):
+            self.hand_idx = 0
+            self.playing = False
+
+    def split(self, idx=-1):
+        if not self.playing:
+            return
+        current_hand = self.hands[self.hand_idx]
+        if len(current_hand.cards) == 2 and current_hand.cards[0] == current_hand.cards[1]:
+            print(f'Player {self.id} is splitting...')
+            card = self.hands.pop(idx).cards[0]
+            self.hands.extend([Hand([card]), Hand([card])])
+        else:
+            print('Can not split.')
+    
+    def double(self):
+        if not self.playing:
+            return
+        self.bet *= 2
+        self.doubled = True
+
+class Dealer:
+    def __init__(self, deck: Deck):
+        self.deck = deck
+        self.hand = Hand()
+    
+    def add_to_hand(self, card):
+        self.hand.add_to_hand(card)
+
+    def deal(self, show=True):
+        return self.deck.draw(show)
+    
+    def play(self):
+        showed = self.hand.reveal()
+        if showed:
+            print(f'Dealer hand after revealing card(s) is {self.hand}')
+        while self.hand.compute_value() < 17:
+            card = self.deal()
+            self.hand.add_to_hand(card)
+            print(f'Dealer added {card} to their hand. Dealer hand is {self.hand}.')
+            time.sleep(1)
+        print(f'Dealer final hand is {self.hand}. The total score is {self.hand.compute_value()}.')
+        if self.hand.compute_value() > 21:
+            print('Dealer went bust!')
+            self.hand.clear()
+        return self.hand.compute_value()
+
+
+class Game:
+    def __init__(self, num_players=1):
+        self.deck = Deck(3)
+        self.players = [Player(id=id) for id in range(num_players)]
+        self.dealer = Dealer(self.deck)
+
+    def setup(self):
+        for player in self.players:
+            player.add_to_hand(self.dealer.deal())
+        self.dealer.add_to_hand(self.dealer.deal())
+
+        for player in self.players:
+            player.add_to_hand(self.dealer.deal())
+        self.dealer.add_to_hand(self.dealer.deal(False))
+
+    def play_round(self):
+        for player in self.players:
+            player.bet = int(input('Place your bet: '))
+        self.setup()
+        print(f'Dealer hand is {self.dealer.hand}.')
+        for player in self.players:
+            print(f'Player {player.id}\'s hand is {player.hands}')
+        time.sleep(2)
+        for player in self.players:
+            playing = True
+            while playing:
+                playing = player.play(None, self.dealer.deal())
+        dealer_score = self.dealer.play()
+        for player in self.players:
+            for hand in player.hands:
+                showed = hand.reveal()
+                if showed:
+                    print(f'Player hand after revealing card(s) is {hand}')
+                if hand.compute_value() > dealer_score:
+                    player.money += player.bet
+                    print(f'Player {player.id} made {player.bet} bucks! Player now has {player.money} money left.')
+                elif hand.compute_value() == dealer_score:
+                    print(f'Player {player.id} stood.')
+                else:
+                    player.money -= player.bet
+                    print(f'Player {player.id} lost {player.bet} bucks! Player now has {player.money} money left.')
+            time.sleep(1)
+
+if __name__ == '__main__':
+    game = Game()
+    game.play_round()
